4f333d65251b23c205d2606961e94e12021f7743
YashSahsani <yashsahsani5@gmail.com>
2020-09-03 09:17:28 +0530

    adding modified get_ip.py script

--- get_ip.py
+++ get_ip.py
@@ -1,18 +1,39 @@
-import grequests
 import threading
-class Test(threading.Thread):
-    def __init__(self,threadID,name,counter):
-        threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
-        self.counter = counter
-    def exception(self, request, exception):
-        print ("Problem: {}: {}".format(request.url, exception))
+import re
+import subprocess
+Flag = False
+def get_ip_():
+    global Flag,ip
+    while(True):
+        if(Flag):
+            return ip
+        else:
+            pass
 
-    def tasync(self):
-        results = grequests.map((grequests.get(u) for u in self.urls), exception_handler=self.exception, size=5)
-        print (results)
-    def run(self):
-        response = os.popen("ping -n 1")
-for i in range(50):
-	Test(i,"Thread-"+str(i),i).start()
+def task(n, cmd):
+    global ip
+    global Flag
+    temp = cmd + str(n)
+    res = subprocess.Popen(("ping -n 1 " + temp), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+    res = res.stdout.read().decode()
+    if 'Destination host unreachable.' in res or 'Request timed out.' in res:
+        pass
+    else:
+        ip.append(temp)
+    if(n == 245):
+        Flag = True
+cmd = "ipconfig"
+cmd = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+temp = re.findall('IPv4 Address.+: (.+)',cmd.stdout.read().decode())
+temp = temp[len(temp)-1].rstrip()
+temp = temp.split('.')
+b1 = int(temp[0])
+b2 = int(temp[1])
+b3 = int(temp[2])
+cmd = str(b1) + "."+ str(b2) + "." + str(b3) + "."
+ip = []
+for i in range(1,255):
+    t = threading.Thread(name = i, target = task, args = (i, cmd))
+    t.daemon = False
+    t.start()
+print(get_ip_())
