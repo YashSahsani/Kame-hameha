aebb7cd2786dfd459dea23091864e3a7a4603893
YashSahsani <yashsahsani5@gmail.com>
2020-09-12 14:23:59 +0530

    updated version

--- aes_decryption.py
+++ aes_decryption.py
@@ -3,13 +3,27 @@ from base64 import b64decode
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import unpad
 import os
-def aes_decrypt(filename):
+def aes_decrypt(filename,method):
+    if(method==1):
+            a = filename.split('/')
+            file1 = ''
+            for i in range(len(a)-1):
+                file1 += a[i]+'/'
+            print(file1)
+            name = a[len(a)-1]
+        elif(method==2):
+            a = filename.split('\\')
+            file1 = ''
+            for i in range(len(a)-1):
+                file1 += a[i]+'\\'
+            print(file1)
+            name=file1[len(a)-1]
     b64 = json.loads(open(filename,'r').read())
     iv = b64decode(b64['iv'])
     ct = b64decode(b64['ciphertext'])
     keys = open('res/key.txt','r').readlines()
     for line in keys:
-        if(filename.split('.')[0] == line.split(":")[0]):
+        if(file1+name.split('.')[0] == line.split(":")[0]):
             key = line.split(":")[1]
     key = b64decode(key)
     cipher = AES.new(key, AES.MODE_CBC, iv)
