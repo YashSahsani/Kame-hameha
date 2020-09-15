ce6baf867aa9f5dddfaba434432710e138ff5c05
YashSahsani <yashsahsani5@gmail.com>
2020-09-15 16:26:14 +0530

    bug fix

--- propagation/wall.bat
+++ propagation/wall.bat
@@ -1,5 +1,7 @@
 reg add "HKEY_CURRENT_USER\control panel\desktop" /v wallpaper /t REG_SZ /d "" /f 
-reg add "HKEY_CURRENT_USER\control panel\desktop" /v wallpaper /t REG_SZ /d "C:\Users\Administrator\GOKU.jpg" /f
+reg add "HKEY_CURRENT_USER\control panel\desktop" /v wallpaper /t REG_SZ /d "C:\Users\Administrator\Desktop\GOKU.jpg" /f
 reg add "HKEY_CURRENT_USER\control panel\desktop" /v WallpaperStyle /t REG_SZ /d 2 /f
+timeout 2>NUL
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters 
+timeout 3>NUL
 exit
